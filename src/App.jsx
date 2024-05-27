812f778132c9db3222a7787de01fa3d22e0a4ca8
kittymiton <kitty.miton@gmail.com>
2024-05-27 17:03:15 +0900

    “HeaderとPostに分割”

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
-import { posts } from './data/posts'
+import { Post } from "./components/post";
+import { Header } from "./components/header";
 
 export const App = () => {
-  // 日付を変換
-  const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    return date.toLocaleDateString('ja-JP');
-  }
+
   return (
-    <ul>
-      {posts.map(post => (
-        <li key={post.id}>
-          <div className="post">
-            <div className="post-info">
-              <p>{formatDate(post.createdAt)}</p>
-              <ul>{post.categories.map(category => (
-                <li key={category}>
-                  {category}
-                </li>
-              ))}
-              </ul>
-            </div>
-            <h1>{post.title}</h1>
-            <p dangerouslySetInnerHTML={{ __html: post.content }} />
-          </div>
-        </li>
-      ))}
-    </ul>
-  )
+    <>
+      <Header />
+      <main>
+        <Post />
+      </main>
+    </>
+  );
 };
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+export const Header = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          Blog
+        </li>
+        <li>
+          お問い合わせ
+        </li>
+      </ul>
+    </nav>
+  )
+
+}
