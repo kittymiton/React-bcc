669b62c23d805becccdcb7c8f0c6b73934842c79
kittymiton <kitty.miton@gmail.com>
2024-06-17 19:51:45 +0900

    refactor:本番以外のファイルを削除

--- src/components/Button copy.jsx
+++
@@ -1,9 +0,0 @@
-export const Button = ({ type, tag, onClick }) => {
-  return (
-    <button
-      type={type}
-      onClick={onClick} // クリックイベントに対するハンドラ関数を実行するようにセット。ハンドラ関数を実行しているのは親。
-    //onClick={() => onClick()}// 関数が他の関数を引数として受け取る必要がある場合や、関数の実行前に特定の条件をチェックする必要がある場合など。
-    >{tag}</button>
-  );
-};
